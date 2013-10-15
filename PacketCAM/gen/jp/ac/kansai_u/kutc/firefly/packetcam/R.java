92a632d711674b8431d8d383b97787cef1e082c4
K-Kensuke <black1993raven@gmail.com>
2013-10-15 18:05:54 +0900

    解像度選択ボタンを追加

--- PacketCAM/gen/jp/ac/kansai_u/kutc/firefly/packetcam/R.java
+++ PacketCAM/gen/jp/ac/kansai_u/kutc/firefly/packetcam/R.java
@@ -26,8 +26,10 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f080000;
-        public static final int action_settings=0x7f080003;
+        public static final int action_settings=0x7f080005;
         public static final int button1=0x7f080002;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080004;
         public static final int surfaceView1=0x7f080001;
     }
     public static final class layout {
@@ -37,10 +39,12 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int INOUT=0x7f050004;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int flash=0x7f050003;
         public static final int hello_world=0x7f050002;
+        public static final int resolution=0x7f050005;
     }
     public static final class style {
         /** 
