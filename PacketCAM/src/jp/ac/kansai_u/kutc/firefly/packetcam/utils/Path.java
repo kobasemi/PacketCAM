39e5a5bd0eee2fa92ed18b93e3c42a57a3b84386
akasaka <asakasa@me.com>
2013-12-07 17:33:41 +0900

    アプリ内で使用するパスをまとめたクラス

---
+++ PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/utils/Path.java
@@ -0,0 +1,14 @@
+package jp.ac.kansai_u.kutc.firefly.packetcam.utils;
+
+/**
+ * アプリ内で使用するPATHの一覧
+ * @author akasaka
+ */
+public class Path {
+    // SDカードのパス
+    public static String SD_PATH = null;
+    // 画像保存フォルダのパス
+    public static String PICFOLDER_PATH = null;
+    // Packetデータ保存フォルダのパス
+    public static String PACKETFOLDER_PATH = null;
+}
