92a8cb3bd6a7fe30180232084bff41941d71f7d0
asakasa <asakasa@me.com>
2014-01-11 23:40:32 +0900

    設定ダイアログの編集 解像度に関する処理の削除 フラッシュのモードをONからトーチに変更

--- PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/setting/SettingDialog.java
+++ PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/setting/SettingDialog.java
@@ -15,8 +15,6 @@ import jp.ac.kansai_u.kutc.firefly.packetcam.opengl.DrawCamera;
 public class SettingDialog{
     final String TAG = "SettingDialog.java";
     Activity activity;
-    // 画像サイズ（height，width）
-    Camera.Size picSize = null;
 
     /**
      * ダイアログを表示する
@@ -25,75 +23,23 @@ public class SettingDialog{
     public void show(Activity a){
         activity = a;
 
-//        CharSequence[] conf = { "パケットの読み込み", "解像度", "フラッシュ" };
 		CharSequence[] conf = { "パケットの読み込み", "フラッシュ" };
 
-
-		final AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-        builder.setTitle("設定");
-        builder.setItems(conf, new DialogInterface.OnClickListener() {
+		final AlertDialog.Builder rootBuilder = new AlertDialog.Builder(activity);
+        rootBuilder.setTitle("設定");
+        rootBuilder.setItems(conf, new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 if(which == 0) {
                     new ReadPcapFileDialog().show(activity);
                 }
-//                if (which == 1) {
-//                    // 解像度
-//                    // 対応する画像サイズのリストを取得
-//                    final List<Camera.Size> supportedPictureSize = DrawCamera.getCamera().getParameters().getSupportedPictureSizes ();
-//                    // 画像サイズの個数を取得
-//                    int numPicItem = supportedPictureSize.size ();
-//
-//                    // さきほど取得した個数をもとに配列を定義
-//                    String[] picHeight = new String[numPicItem];
-//                    String[] picWidth = new String[numPicItem];
-//                    final String[] pic = new String[numPicItem];
-//
-//                    for (int i = 0; i < supportedPictureSize.size (); i++)
-//                    {
-//                        picSize = supportedPictureSize.get (i);
-//                        picHeight[i] = String.valueOf (picSize.height);
-//                        picWidth[i] = String.valueOf (picSize.width);
-//
-//                        // 縦サイズと横サイズの最大公約数を求める
-//                        int gcd = GetGCD.getGCD(picSize.width, picSize.height);
-//
-//                        String aspWidth = String.valueOf (picSize.width / gcd);
-//                        String aspHeight = String.valueOf (picSize.height / gcd);
-//
-//                        pic[i] = picWidth[i] + " : " + picHeight[i] + "（" + aspWidth + " ： " + aspHeight + "）";
-//                    }
-//
-//
-//                    final AlertDialog.Builder builder2 = new AlertDialog.Builder (activity);
-//                    builder2.setTitle ("解像度");
-//                    builder2.setItems (pic, new DialogInterface.OnClickListener ()
-//                    {
-//                        public void onClick (DialogInterface dialog, int which)
-//                        {
-//                            Camera.Parameters parameter = DrawCamera.getCamera().getParameters();
-//
-//                            picSize = supportedPictureSize.get (which);
-//
-//                            Toast.makeText(activity, "画像サイズを" + pic[which] + "に設定しました．", Toast.LENGTH_SHORT).show ();
-//
-//                            parameter.setPictureSize (picSize.width, picSize.height);
-//                            DrawCamera.getCamera().setParameters(parameter);
-//                            // 設定ファイルに保存
-//                            SettingsManager.getInstance().setResolution(picSize.width, picSize.height);
-//                        }
-//                    });
-//                    builder2.show();
-//                }
                 if (which == 1) {
                     // フラッシュ
-//                    CharSequence[] conf3 = { "強制発光", "自動", "OFF" };
-					CharSequence[] conf3 = { "強制発光", "OFF" };
+					CharSequence[] flashStatus = { "強制発光", "OFF" };
 
-
-					AlertDialog.Builder builder3 = new AlertDialog.Builder (activity);
-                    builder3.setTitle ("フラッシュ");
-                    builder3.setItems (conf3, new DialogInterface.OnClickListener ()
+					AlertDialog.Builder flashBuilder = new AlertDialog.Builder (activity);
+                    flashBuilder.setTitle ("フラッシュ");
+                    flashBuilder.setItems (flashStatus, new DialogInterface.OnClickListener ()
                     {
                         public void onClick (DialogInterface dialog, int which)
                         {
@@ -101,22 +47,13 @@ public class SettingDialog{
                             {
                                 // 強制発光処理
                                 Camera.Parameters parameters = DrawCamera.getCamera().getParameters ();
-                                parameters.setFlashMode (Camera.Parameters.FLASH_MODE_ON);
+                                //TODO: TORCHに対応しているかどうかチェック
+                                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
                                 DrawCamera.getCamera().setParameters (parameters);
                                 Toast.makeText (activity, "Flashを強制発光モードにしました", Toast.LENGTH_SHORT).show ();
                                 // 設定ファイルに保存
-                                SettingsManager.getInstance().setFlashStatus(Camera.Parameters.FLASH_MODE_ON);
+                                SettingsManager.getInstance().setFlashStatus(Camera.Parameters.FLASH_MODE_TORCH);
                             }
-//                            if (which == 1)
-//                            {
-//                                // 自動発光処理
-//                                Camera.Parameters parameters = DrawCamera.getCamera().getParameters ();
-//                                parameters.setFlashMode (Camera.Parameters.FLASH_MODE_AUTO);
-//                                DrawCamera.getCamera().setParameters (parameters);
-//                                Toast.makeText (activity, "Flashを自動モードにしました", Toast.LENGTH_SHORT).show();
-//                                // 設定ファイルに保存
-//                                SettingsManager.getInstance().setFlash(Camera.Parameters.FLASH_MODE_AUTO);
-//                            }
                             if (which == 1)
                             {
                                 // フラッシュOFF処理
@@ -129,10 +66,10 @@ public class SettingDialog{
                             }
                         }
                     });
-                    builder3.show ();
+                    flashBuilder.show ();
                 }
             }
         });
-        builder.show();
+        rootBuilder.show();
     }
 }
