784185c10f9dddec1a4f62be7828823b22e10945
asakasa <asakasa@me.com>
2014-01-12 18:10:21 +0900

    設定ダイアログからフラッシュの項目を削除

--- PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/setting/SettingDialog.java
+++ PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/setting/SettingDialog.java
@@ -10,7 +10,6 @@ import jp.ac.kansai_u.kutc.firefly.packetcam.utils.SharedPreferencesManager;
 
 /**
  * 設定ダイアログに関するクラス
- * TODO: トーストを削除して，選択されてるものを分かりやすいようにする
  * @author akasaka
  */
 public class SettingDialog{
@@ -24,7 +23,7 @@ public class SettingDialog{
     public void show(Activity a){
         activity = a;
 
-		CharSequence[] conf = { "パケットの読み込み", "フラッシュ" };
+		CharSequence[] conf = { "パケットの読み込み"};
 
 		final AlertDialog.Builder rootBuilder = new AlertDialog.Builder(activity);
         rootBuilder.setTitle("設定");
@@ -33,41 +32,6 @@ public class SettingDialog{
             public void onClick(DialogInterface dialog, int which) {
                 if(which == 0) {
                     new ReadPcapFileDialog().show(activity);
-                }
-                if (which == 1) {
-                    // フラッシュ
-					CharSequence[] flashStatus = { "強制発光", "OFF" };
-
-					AlertDialog.Builder flashBuilder = new AlertDialog.Builder (activity);
-                    flashBuilder.setTitle ("フラッシュ");
-                    flashBuilder.setItems (flashStatus, new DialogInterface.OnClickListener ()
-                    {
-                        public void onClick (DialogInterface dialog, int which)
-                        {
-                            if (which == 0)
-                            {
-                                // 強制発光処理
-                                Camera.Parameters parameters = DrawCamera.getCamera().getParameters ();
-                                //TODO: TORCHに対応しているかどうかチェック
-                                parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-                                DrawCamera.getCamera().setParameters(parameters);
-                                Toast.makeText (activity, "Flashを強制発光モードにしました", Toast.LENGTH_SHORT).show ();
-                                // 設定ファイルに保存
-                                SharedPreferencesManager.getInstance().setFlashStatus(Camera.Parameters.FLASH_MODE_TORCH);
-                            }
-                            if (which == 1)
-                            {
-                                // フラッシュOFF処理
-                                Camera.Parameters parameters = DrawCamera.getCamera().getParameters ();
-                                parameters.setFlashMode (Camera.Parameters.FLASH_MODE_OFF);
-                                DrawCamera.getCamera().setParameters (parameters);
-                                Toast.makeText (activity, "FlashをOFFにしました", Toast.LENGTH_SHORT).show();
-                                // 設定ファイルに保存
-                                SharedPreferencesManager.getInstance().setFlashStatus(Camera.Parameters.FLASH_MODE_OFF);
-                            }
-                        }
-                    });
-                    flashBuilder.show ();
                 }
             }
         });
