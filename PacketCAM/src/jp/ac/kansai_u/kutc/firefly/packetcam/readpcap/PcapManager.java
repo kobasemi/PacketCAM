2f9f0d6eeb3170b993bf2eef68256b44e35a687a
asakasa <asakasa@me.com>
2014-01-18 07:07:29 +0900

    スレッドの速度を変更

--- PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/readpcap/PcapManager.java
+++ PacketCAM/src/jp/ac/kansai_u/kutc/firefly/packetcam/readpcap/PcapManager.java
@@ -97,6 +97,8 @@ public class PcapManager implements Runnable{
             setPacketsToPacketList();
         } catch (IOException e) {
             Log.d(TAG, "FAILED TO OPEN");
+        }catch(IndexOutOfBoundsException e){
+            Log.d(TAG, "FAILED TO OPEN");
         }
     }
 
@@ -218,7 +220,7 @@ public class PcapManager implements Runnable{
             // 復帰時にシャットダウンしていた場合
             executor = Executors.newSingleThreadScheduledExecutor();
         // this: スレッド，1000: 周期，TimeUnit.MILLSECONDS: ミリ秒単位
-        future = executor.scheduleAtFixedRate(this, 0, 100, TimeUnit.MILLISECONDS);
+        future = executor.scheduleAtFixedRate(this, 0, 250, TimeUnit.MILLISECONDS);
     }
     /**
      * スレッド処理を一時停止する
